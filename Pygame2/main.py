69af31f68fb7152f8516a9c2e7b4aa08e7780503
Lau Rivero <A01274144@itesm.mx>
2021-04-02 10:58:35 -0600

    Draw
    Create multiple shapes through loops

--- Pygame2/main.py
+++ Pygame2/main.py
@@ -22,9 +22,13 @@ while True:
 	# Color de fondo
 	WIN.fill(WHITE)
 	# ----- Zona de Dibujo
-	pygame.draw.line(WIN, GREEN, (0, 100), (200, 300), 5)
-	pygame.draw.rect(WIN, BLACK, (100, 100, 80, 80))
-	pygame.draw.circle(WIN, RED, (400,250), 100)
+	#pygame.draw.line(WIN, GREEN, (0, 100), (200, 300), 5)
+	#pygame.draw.rect(WIN, BLACK, (100, 100, 80, 80))
+	#pygame.draw.circle(WIN, RED, (400,250), 100)
+	# iniciar, finalizar, i++
+	for x in range(100,700,60):
+		pygame.draw.rect(WIN, BLACK, (x, 230, 50, 50))
+		pygame.draw.line(WIN, GREEN, (x, 0), (x, 100), 5)
 	# ----- Zona de Dibujo
 
 	# Actualizar pantalla
