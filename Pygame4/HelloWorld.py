77dee2ce51b3a4668d9e78a56a35423055b500f3
Lau Rivero <A01274144@itesm.mx>
2021-04-04 19:09:29 -0500

    Text Movement
    Text bouncing in x and y directions in the screen

--- Pygame4/HelloWorld.py
+++ Pygame4/HelloWorld.py
@@ -14,11 +14,30 @@ WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 
 FONT = pygame.font.SysFont("Berlin Sans FB", 30)
 HelloWorld = FONT.render("Hello World", False, DARK_BLUE, LIGHT_BLUE)
+HelloWorld_size = HelloWorld.get_size()
+
+x, y = 0, 0
+direction_x = 1
+direction_y = 1
+
+CLOCK = pygame.time.Clock()
+
 while True:
+	CLOCK.tick(40)
 
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			sys.exit()
 	
-	WIN.blit(HelloWorld,(0, 0))
+	WIN.fill(BLACK)
+
+	WIN.blit(HelloWorld,(x, y))
+
+	x += 5 * direction_x
+	y += 5 * direction_y
+	if x + HelloWorld_size[0] > 800 or x <= 0:
+		direction_x *= -1
+	if y + HelloWorld_size[1] > 600 or y <= 0:
+		direction_y *= -1
+
 	pygame.display.update()
